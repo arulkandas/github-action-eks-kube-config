c7656906b6e9775d386ffc7976ede488ced23e1a
arulkandas <arulkumar.kandasamy@capgemini.com>
2022-12-02 16:34:58 +0000

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,10 @@ if [ -n "${AWS_REGION}" ]; then
   export AWS_DEFAULT_REGION="${AWS_REGION}"
 fi
 
+if [ -n "${CLUSTER_NAME}" ]; then
+  export CLUSTER_NAME="${CLUSTER_NAME}"
+fi
+
 aws eks update-kubeconfig --name "${CLUSTER_NAME}"
 
 chmod 600 ~/.kube/config
